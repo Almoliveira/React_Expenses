a9b3f9d5b1a09692e3ce118bf4603b8303e402de
André Luís Martins de Oliveira <almoliveira.info@gmail.com>
2021-10-24 17:37:31 -0300

    Section 5 Finished!

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -2,11 +2,13 @@ import React, { useState } from "react";
 
 import Card from "../UI/Card";
 import "./Expenses.css";
+import ExpensesChart from "./ExpensesChart";
 import ExpensesFilter from "./ExpensesFilter";
 import ExpensesList from "./ExpensesLIst";
 
 function Expenses(props) {
   const [selectedYear, setSelectedYear] = useState("2021");
+
   const filterChangedHandler = (year) => {
     console.log(year);
     setSelectedYear(year);
@@ -18,11 +20,13 @@ function Expenses(props) {
 
   return (
     <div>
+     
       <Card className="expenses">
         <ExpensesFilter
           onFilterChanged={filterChangedHandler}
           defaultYear={selectedYear}
         />
+         <ExpensesChart expenses={filteredExpenses}/>
         <ExpensesList items={filteredExpenses}/>
       </Card>
     </div>
