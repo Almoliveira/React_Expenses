7e73cea5fd05a6852552b64a52a30358fd8486f0
André Luís Martins de Oliveira <almoliveira.info@gmail.com>
2021-10-24 09:03:50 -0300

    NewExpense and ExpenseForm has been added!

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
+import React from 'react';
+
+import NewExpense from './components/NewExpense/NewExpense';
 import Expenses from "./components/Expenses/Expenses";
+
+
 
 function App() {
   const expenses = [
@@ -25,7 +30,7 @@ function App() {
 
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense />
       <Expenses items={expenses} />
     </div>
   );
