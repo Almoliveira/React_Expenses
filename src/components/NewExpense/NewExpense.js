c8f02d6f4fcfb0e17c5c280d2a44b1eb9e0e6b54
André Luís Martins de Oliveira <almoliveira.info@gmail.com>
2021-10-24 10:19:25 -0300

    Added basic child-parent communication.

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -3,10 +3,22 @@ import "./NewExpense.css";
 
 import ExpenseForm from "./ExpenseForm.js";
 
-const NewExpense = () => {
+const NewExpense = (prop) => {
+
+  const saveExpenseDataHandler = (enteredExpenseData) => {
+    const expenseData = {
+      ...enteredExpenseData,
+      id: Math.random().toString()
+    };
+
+    prop.onAddExpenseHandler(expenseData);
+    //console.log(expenseData);
+    
+  };
+
   return (
     <div className="new-expense">
-     <ExpenseForm />
+     <ExpenseForm onSaveExpenseData={saveExpenseDataHandler}/>
     </div>
   );
 };
